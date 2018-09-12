64c98819464ddf26873c4dd8ae76d409acc58d69
Eagan Martin <mece.martinece@gmail.com>
2018-09-12 13:34:12 +0800

    Added database helper for sqlite;

---
+++ app/src/main/java/com/fujiyama/pulp/sqlitenotesapp/database/DatabaseHelper.java
@@ -0,0 +1,28 @@
+package com.fujiyama.pulp.sqlitenotesapp.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.fujiyama.pulp.sqlitenotesapp.database.model.Note;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "notesdb";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(Note.CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + Note.TABLE_NAME);
+
+        onCreate(db);
+    }
+}
